739000e6716ad9aa8ab94c31d20c5ee08538d171
emmanuelle.karimou <emmanuelle.karimou@monlycee.net>
2026-06-22 16:05:06 +0200

    to do list 5 COMPLET

---
+++ ltùoij.py
@@ -0,0 +1,13 @@
+message_chiffre=[]
+for caractere in message :
+    if caractere =="e"
+        message_chiffre.join(random.choice(["$", "£"]))
+    elif caractere in cle :
+        message_chiffre.join(dict.get('caractere'))
+    elif caractere == " ":
+        message_chiffre.join("e")
+    else :
+    dictionnaire.get(cle, valeur_par_defaut)
+        message_chiffre.append(nouveau_symbole)
+ message[::-1]
+
