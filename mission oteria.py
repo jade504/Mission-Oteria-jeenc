a2f8406e98afae6a0dc8d08ff9701b50ba974fff
eloise.nocent <eloise.nocent@monlycee.net>
2026-06-22 15:55:17 +0200

    test

---
+++ mission oteria.py
@@ -0,0 +1,9 @@
+fichier="chiffrement.txt"
+chiffrement=[]
+try :
+    with open(fichier,"r") as f :
+        fichier=f.read().splitlines()
+        pass
+except FileNotFoundError:
+    print("Ce fichier est vide")
+key={"a":"¤","b":":","c":">","d":"<","e":"$" or "£","f":"~","g":"/","h":".","i":"¨","j":"#","k":"+","l":"=","m":"_","n":"-","o":"°","p":"^","q":"{","r":"@","s":"§","t":"&","u":"%","v":"?","w":"!","x":"µ","y":"²","z":"*"}
