594abb18bf6959952bf6469ff929b74b9947da30
jade504 <jade.coutocabral@gmail.com>
2026-06-22 16:18:02 +0200

    version 3

--- chifreur_jeenc.py
+++ chifreur_jeenc.py
@@ -1,14 +1,9 @@
 
 
-
-
-
-
 import random
 import unicodedata
 
-
-cle = {"a":"¤", "b" : ":" }
+cle={"a":"¤","b":":","c":">","d":"<","f":"~","g":"/","h":".","i":"¨","j":"#","k":"+","l":"=","m":"_","n":"-","o":"°","p":"^","q":"{","r":"@","s":"§","t":"&","u":"%","v":"?","w":"!","x":"µ","y":"²","z":"*"}
 
 message = input("quel est le message que vous voulez chiffrer?").lower()
 
@@ -24,3 +19,8 @@ for caractere in message:
     resultat.append(nouveau_symbole)
 resultat = "".join(resultat)
 
+message = message.replace(" ", "e")
+
+
+message_chiffre = resultat[::-1]
+print("Message chiffré :", message_chiffre)
